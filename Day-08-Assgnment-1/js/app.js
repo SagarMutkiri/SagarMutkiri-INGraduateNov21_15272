92a5a430477fc6bd1a0b57697d1ddf36292fbe3f
SagarMutkiri <93123228+SagarMutkiri@users.noreply.github.com>
2021-12-16 20:06:32 +0530

    second

---
+++ Day-08-Assgnment-1/js/app.js
@@ -0,0 +1,13 @@
+const book = {
+    bookid:101,
+    bookname:"Rich_Dad_Poor_Dad",
+    authorname:"Robert Kiyosaki"
+}
+
+document.getElementById("demo").innerHTML = "Book Id = " + book.bookid + "  Book Name = " + book.bookname + "  Author Name = " + book.authorname ;
+
+
+
+// console.log(book.bookid);
+// console.log(book.bookname);
+// console.log(book.authorname);
